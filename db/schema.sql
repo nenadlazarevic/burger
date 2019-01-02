edf7834d3f19602410d5a67d1e2ca152d41090b5
Laza <postlaza@gmail.com>
2019-01-02 15:36:25 -0800

    set

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+drop database if exists burgers_db;
+
+create database  burgers_db;
+
+use  burgers_db;
+
+create table burgers (
+
+id int(10) not null auto_increment,
+burger_name varchar(30) not null,
+devoured boolean,
+primary key (id)
+
+
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES("cheeseburger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES("Veggie_Burger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES("BigMac", 1);
