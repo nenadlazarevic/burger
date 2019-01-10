fa1fc889f19b7a72b5fa06bbecb8cbb5ed331d1a
Laza <postlaza@gmail.com>
2019-01-09 18:19:06 -0800

    test5

--- db/schema.sql
+++ db/schema.sql
@@ -3,11 +3,13 @@ drop database if exists burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (id)
+
+
+CREATE TABLE burgers(
+ id INT NOT NULL AUTO_INCREMENT,
+ burger_name VARCHAR(100) NOT NULL,
+ devoured BOOLEAN DEFAULT false,
+ createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ PRIMARY KEY(id)
+
 );
